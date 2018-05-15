0eebfe67fefdfe7ed8b8c0d5d97cc759b1dccb68
Josh Mulloy <joshmulloy1@gmail.com>
2018-05-15 17:25:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,12 +18,12 @@ function add2(n) {
   
 }
 
-var funkyFunction = function() {
-  return function() {
+var funkyFunction = function outsidefunction() {
+  return function insidefunction() {
     return "FUNKY!"
   }
 }
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
-var theFunk = FUNKY
+var theFunk = funckyFunction()()
